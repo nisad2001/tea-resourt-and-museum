5feb55db1dac77375d4213f7b08eb81d879ae279
nisad2001 <nisadnn2001@gmail.com>
2026-07-09 17:54:55 +0600

    Made images responsive using max-width and aspect ratio

--- src/component/ContactGetInTouch.jsx
+++ src/component/ContactGetInTouch.jsx
@@ -4,14 +4,13 @@ import { GiChestnutLeaf } from "react-icons/gi";
 import { IoCallOutline } from "react-icons/io5";
 import { LuSendHorizontal } from "react-icons/lu";
 import { MdOutlineMail } from "react-icons/md";
-import { TbWorldWww } from "react-icons/tb";
 import { VscLocation } from "react-icons/vsc";
 
 const ContactGetInTouch = () => {
   return (
     <div className='max-w-7xl mx-auto px-4 py-10'>
       <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-9 gap-8'>
-        <div className='lg:col-span-3 p-4'>
+        <div className='lg:col-span-3 p-4 transition-transform duration-300 hover:scale-[1.02] cursor-pointer'>
           <div className='flex items-center gap-2'>
             <GiChestnutLeaf className='text-green-500 text-3xl animate-pulse flex-shrink-0' />
             <h3 className='text-green-700 font-bold text-3xl md:text-4xl'>
@@ -48,29 +47,43 @@ const ContactGetInTouch = () => {
                 <p className='text-sm text-gray-600'>info@tearesort.com</p>
               </div>
             </div>
+            <div className='flex items-start gap-3'>
+              <CiClock2 className='text-green-600 bg-base-300 rounded-full p-2 text-4xl flex-shrink-0' />
+              <div>
+                <h6 className='font-semibold text-[18px]'>Office Hours</h6>
+                <p className='text-sm text-gray-600'>9:00 AM - 6:00 PM</p>
+              </div>
+            </div>
+            <div className='flex items-start gap-3'>
+              <CiClock2 className='text-green-600 bg-base-300 rounded-full p-2 text-4xl flex-shrink-0' />
+              <div>
+                <h6 className='font-semibold text-[18px]'>Website</h6>
+                <p className='text-sm text-gray-600'>www.tearesort.com</p>
+              </div>
+            </div>
           </div>
         </div>
 
-        <div className='lg:col-span-2 bg-white rounded-2xl shadow-lg border border-gray-100 flex flex-col p-4'>
+        <div className='lg:col-span-2 bg-white rounded-2xl shadow-lg border border-gray-100 flex flex-col transition-transform duration-300 hover:scale-[1.02] cursor-pointer'>
           <img
             src='/asset/contact-getintouch.jpg'
             alt='Contact'
-            className='w-full h-48 object-cover rounded-xl mb-4'
+            className='w-full h-64 object-cover rounded-xl mb-4'
           />
-          <h3 className='text-2xl font-bold text-gray-800 px-2'>Find Us</h3>
-          <p className='text-gray-600 text-base leading-relaxed px-2 mb-4'>
+          <h3 className='text-2xl font-bold text-gray-800 p-2'>Find Us</h3>
+          <p className='text-gray-600 text-base leading-relaxed p-2'>
             Nestled in the heart of lush tea gardens. Our resort is easily
             accessible.
           </p>
           <div className='mt-auto p-2'>
-            <button className='bg-green-900 text-white w-full py-3 rounded-xl font-bold flex items-center justify-center gap-2 hover:bg-green-800 transition uppercase'>
+            <button className='bg-green-900 text-white w-full py-3 rounded-xl font-bold flex items-center justify-center gap-2 hover:bg-green-800 transition uppercase hover:cursor-pointer'>
               GET direction <FaArrowRight />
             </button>
           </div>
         </div>
 
-        <div className='lg:col-span-4 p-4'>
-          <div className='p-8 bg-white border border-gray-100 rounded-2xl shadow-sm'>
+        <div className='lg:col-span-4 p-4 transition-transform duration-300 hover:scale-[1.02] cursor-pointer'>
+          <div className='lg:p-8 bg-white border border-gray-100 rounded-2xl shadow-sm h-full'>
             <div className='flex items-center gap-2 mb-2'>
               <GiChestnutLeaf className='text-green-800 text-3xl' />
               <h2 className='text-3xl font-bold text-green-900'>
@@ -103,7 +116,7 @@ const ContactGetInTouch = () => {
                 className='w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-600'
               ></textarea>
 
-              <button className='bg-green-900 w-full text-white py-3 rounded-lg font-bold flex items-center justify-center gap-2 hover:bg-green-800 transition uppercase'>
+              <button className='bg-green-900 w-full text-white py-3 rounded-lg font-bold flex items-center justify-center gap-2 hover:bg-green-800 transition uppercase hover:cursor-pointer'>
                 SEND MESSAGE <LuSendHorizontal />
               </button>
             </form>
